07c2a4ebaeddb342ec833e8e08b350ce6875a2bb
Divyanshu Sahu <dsahu1997@gmail.com>
2019-07-31 20:26:22 +0530

    heroku config added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path")
 
 const users = require("./routes/api/users");
 
@@ -25,6 +26,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/api/users", users);
 
-const port = 5000;
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('frontend/build'));
+
+	app.get("*", (req, res) => {
+		req.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
